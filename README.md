242f9cca27dc9bd9f6c6aadc17f776a13aa0629d
Christopher Jaime <cjaime@christophersmbp.home>
2017-11-05 17:28:22 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calendar Question
+First Question
+
+Design a relation database schema for a calendar application.
+Such that events can be defined with a name and a start time and end time
+Customers are defined with a firstname and a lastname
+And you should be able to assign customers to events
---
+++ src/main/java/com/anaplan/interview/Application.java
@@ -0,0 +1,43 @@
+package com.anaplan.interview;
+
+import com.anaplan.interview.domain.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL)");
+
+        jdbcTemplate.execute("DROP TABLE events IF EXISTS");
+
+        jdbcTemplate.execute("CREATE TABLE events(" +
+                "id SERIAL)");
+
+    }
+}
